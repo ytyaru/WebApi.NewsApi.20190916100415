60c60e7c205e7669b38fbc3c18f51a6c5b84bb54
ytyaru <ytyaru@outlook.jp>
2019-09-16 11:42:23 +0900

    コードを縮小

--- src/run.sh
+++ src/run.sh
@@ -12,27 +12,25 @@ request() {
 	 -H "X-Api-Key:${NEWS_API_KEY}"
 }
 # $1: NewsApi結果JSONファイルパス
-is_run() {
-	[ ! -f "${1}" ] && echo 'true' || is_not_today "${1}";
-}
+is_run() { [ ! -f "$1" ] && echo 'true' || is_not_today "$1"; }
 # $1: NewsApi結果JSONファイルパス
 is_not_today() {
 	# ファイルが存在し、更新日時が今日なら falseを返す
-	updated="`date +"%Y-%m-%d" -r "${1}"`"
+	updated="`date +"%Y-%m-%d" -r "$1"`"
 	[ "$updated" = "`date +"%Y-%m-%d"`" ] && echo 'false' || { rename_file "$1"; echo 'true'; }
 }
 # $1: NewsApi結果JSONファイルパス
 format_json() {
 	local name="_`basename "$1"`"
 	local dir="`dirname "$1"`"
-	cat "${1}" | python3 -c 'import sys,json;print(json.dumps(json.loads(sys.stdin.read()),indent=4,ensure_ascii=False))' > "${dir%/}/${name}"
+	cat "$1" | python3 -c 'import sys,json;print(json.dumps(json.loads(sys.stdin.read()),indent=4,ensure_ascii=False))' > "${dir%/}/${name}"
 }
 # TodayNews.jsonファイルが既存だが更新日時が今日でないときYYYYmmdd.jsonにリネームする。
 # $1: NewsApi結果JSONファイルパス
 rename_file() {
 	local today="`date +"%Y%m%d"`"
-	local updated="`date +"%Y%m%d" -r "${1}"`"
-	[ "$updated" != "$today" ] && mv "${1}" "${updated}.json"
+	local updated="`date +"%Y%m%d" -r "$1"`"
+	[ "$updated" != "$today" ] && mv "$1" "${updated}.json"
 }
 run() {
 	local TODAY_NEWS="${TODAY_NEWS:-TodayNews.json}"
